9ce0e63f84fa73978a17acafd5578f6aa2c4be4c
tomiijournal <freakyyytommass@proton.me>
2026-02-18 22:08:33 +0100

    new update

---
+++ src/main.js
@@ -0,0 +1,55 @@
+const $previewIframe = document.getElementById('iframe');
+const filenameinput = document.getElementById("filenameinput")
+
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+async function fetchText(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const text = await response.text();
+    return text;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
+async function handleIframeLoad() {
+  const filename = GetURLParameter("filename");
+
+  if (!filename) {
+    alert("No filename specified!")
+  }
+
+  const markdownText = await fetchText("src/"+filename+".md");
+  if (markdownText) {
+    $previewIframe.contentDocument.body.innerHTML = marked.parse(markdownText);
+    console.log('Markdown rendered');
+  } else {
+    alert("Filename was not found!");
+  }
+}
+
+filenameinput.addEventListener("keyup", ({key}) => {
+    if (key === "Enter") {
+        console.log(filenameinput.value)
+        window.location.href = location.origin + location.pathname+"?filename="+filenameinput.value;
+    }
+})
+$previewIframe.addEventListener('load', handleIframeLoad);
---
+++ src/zapis.md
@@ -0,0 +1,39 @@
+ # Sociologie
+
+## Test - otázky
+- Sociologie - podává celkový obraz společnosti
+- Sociální problém - to co společnost momentálně považuje za problém 
+- Socializace - začleňování se do společnosti
+- Stratifikace - rozvrstvení společnosti
+- Sociální vrstva 
+- Sociální mobilita - vertikální (přesun z jedné vrstvy do druhé), horizontální (změna území)
+
+## Sociální vztahy a struktury
+- je vztah jedinců, jejich pozic a rolí = uspořádání vztahů ve společnosti
+
+### Sociální role
+- dané postavení v určité společnosto (rolí máme více) a chování, které se v daném postavení od nás očekává
+
+### Sociální pozice (status)
+- hodnota postavení (role), je svázáno s nějakým výkonem a opět chováním
+
+
+___
+
+
+- **formální vztah** = pracovní, cizí lidé (vykání)
+- **neformální vzth** = kamarádi, rodina (tykání)
+
+### Sociální kontrola
+> reálné chování člověka a očekávané chování v dané roli
+1. vnější (ze strany druhých)
+2. vnitřní (sebekontrola)
+
+> po stabilitu sociálního řadu je vždy vytvořen systém tvz. sankcí
+- pozitivní = odměna<br>
+- negativní = trest</p>
+
+> reakce jedince na sociální kontrolu
+1. konformismus - člověk se přizpůsobí a adaptuje
+2. inovace - přijdu s nečím novým, budu volit nové prostředky zařízení se do skupiny
+3. únik - ignoruji danou skupinu nebo společnost, uzavření se vůči pravidlům
